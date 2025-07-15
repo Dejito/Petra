f2be1196a82f9729691249be3ee0cd2c9f450296
dejito <Deerealboy@gmail.com>
2025-07-15 11:01:26 +0100

    setting up ktor http library

--- app/src/main/java/com/mobile/petra/data/remote/PetraRepositoryImpl.kt
+++ app/src/main/java/com/mobile/petra/data/remote/PetraRepositoryImpl.kt
@@ -1,9 +1,14 @@
 package com.mobile.petra.data.remote
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.headers
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-\
+
 class PetraRepositoryImpl: PetraRepository {
 
     private val json = Json {
@@ -14,5 +19,33 @@ class PetraRepositoryImpl: PetraRepository {
     }
 
     private lateinit var httpClient: HttpClient
+
+    private suspend fun initHttpClient() {
+
+        httpClient = HttpClient {
+            install(DefaultRequest) {
+                headers {
+                }
+            }
+//            install(Logging) {
+//                logger = Logger.SIMPLE
+//                level = LogLevel.ALL
+//            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = 60000
+                connectTimeoutMillis = 60000
+                socketTimeoutMillis = 60000
+            }
+            install(ContentNegotiation) {
+                json(json)
+            }
+        }
+    }
+
+    private suspend fun ensureHttpClientInitialized() {
+        if (!this::httpClient.isInitialized) {
+            initHttpClient()
+        }
+    }
 
 }
