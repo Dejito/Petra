abaf88ab6c646f6a6db6b514abf7f5cf74213238
dejito <Deerealboy@gmail.com>
2025-08-04 10:07:19 +0100

    implemented fetch-products in ProductViewModel

--- app/src/main/java/com/mobile/petra/presentation/viewmodel/product/ProductViewModel.kt
+++ app/src/main/java/com/mobile/petra/presentation/viewmodel/product/ProductViewModel.kt
@@ -1,7 +1,23 @@
 package com.mobile.petra.presentation.viewmodel.product
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mobile.petra.data.model.response.ProductResponse
+import com.mobile.petra.data.remote.PetraRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ProductViewModel() : ViewModel() {
+class ProductViewModel(private  val petraRepository: PetraRepository) : ViewModel() {
+
+    fun fetchProducts(
+        onSuccess: (ProductResponse) -> Unit,
+        onFailure: (String) -> Unit
+    ){
+        viewModelScope.launch(context = Dispatchers.IO) {
+            petraRepository.fetchProduct(
+                onSuccess, onFailure
+            )
+        }
+    }
 
 }
