01160f052fb1af817b306cf18d3990709f7dd3a5
dejito <Deerealboy@gmail.com>
2025-08-09 17:40:52 +0100

    parsing products-response list to LazyVerticalGrid

--- app/src/main/java/com/mobile/petra/presentation/views/products/ProductsComponents.kt
+++ app/src/main/java/com/mobile/petra/presentation/views/products/ProductsComponents.kt
@@ -1,15 +1,10 @@
 package com.mobile.petra.presentation.views.products
 
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,14 +19,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -41,15 +30,13 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.mobile.petra.R
-import com.mobile.petra.data.local.Product
+import com.mobile.petra.data.model.response.ProductResponse
 
 @Composable
 fun ProductItem(
-    product: Product,
+    product: ProductResponse,
     modifier: Modifier,
 //    product: Product,
 ) {
@@ -92,7 +79,8 @@ fun ProductItem(
                     Icon(
                         imageVector = Icons.Default.Favorite,
                         contentDescription = null,
-                        tint = if (product.isFavorite) Color.Red else Color.Gray,
+                        tint =  Color.Gray,
+//                        if (product.isFavorite) Color.Red else
                         modifier = Modifier.size(18.dp)
                     )
                 }
